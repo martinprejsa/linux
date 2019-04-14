84e9a28c7faf6a947a117fbf219747232360e9ef
Andreas Färber <afaerber@suse.de>
2019-04-15 01:01:36 +0200

    net: fsk: si443x: Implement PHY and {get,set}_freq
    Allocate an fsk_phy and implement the get_freq and set_freq callbacks.
    
    v2:
    * Fixed do_div() build error (kbuild bot)
    
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- drivers/net/fsk/si443x.c
+++ drivers/net/fsk/si443x.c
@@ -5,12 +5,153 @@
  * Copyright (c) 2019 Andreas Färber
  */
 
+#include <linux/bitops.h>
 #include <linux/module.h>
 #include <linux/netdevice.h>
 #include <linux/of.h>
 #include <linux/of_device.h>
 #include <linux/regmap.h>
 #include <linux/spi/spi.h>
+#include <net/cfgfsk.h>
+
+#define REG_FO1		0x73
+#define REG_FO2		0x74
+#define REG_FO2_FO_MASK		GENMASK(1, 0)
+
+#define REG_FBSEL	0x75
+#define REG_FBSEL_FB_MASK	GENMASK(4, 0)
+
+#define REG_FC1		0x76
+#define REG_FC2		0x77
+#define REG_FHCH	0x79
+#define REG_FHS		0x7a
+
+struct si443x_priv {
+	struct regmap *regmap;
+};
+
+static int si443x_fsk_get_freq(struct fsk_phy *phy, u32 *val)
+{
+	struct regmap *regmap = dev_get_drvdata(phy->dev);
+	unsigned int fo1, fo2, fbsel, fc1, fc2, fhch, fhs;
+	u16 tmp, fc;
+	s16 fo;
+	u32 fcarrier, fb, hbsel;
+	int ret;
+
+	ret = regmap_read(regmap, REG_FO1, &fo1);
+	if (ret)
+		return ret;
+
+	ret = regmap_read(regmap, REG_FO2, &fo2);
+	if (ret)
+		return ret;
+
+	tmp = (((u16)fo2 & REG_FO2_FO_MASK) << 8) | fo1;
+	if (tmp & BIT(9))
+		/* sign-extend from 10-bit to 16-bit */
+		tmp |= GENMASK(15, 10);
+	fo = (s16)tmp;
+
+	ret = regmap_read(regmap, REG_FBSEL, &fbsel);
+	if (ret)
+		return ret;
+	fb = fbsel & REG_FBSEL_FB_MASK;
+	hbsel = (fbsel >> 5) & 0x1;
+
+	ret = regmap_read(regmap, REG_FC1, &fc1);
+	if (ret)
+		return ret;
+
+	ret = regmap_read(regmap, REG_FC2, &fc2);
+	if (ret)
+		return ret;
+
+	fc = ((u16)fc1 << 8) | fc2;
+
+	ret = regmap_read(regmap, REG_FHCH, &fhch);
+	if (ret)
+		return ret;
+
+	ret = regmap_read(regmap, REG_FHS, &fhs);
+	if (ret)
+		return ret;
+
+	fcarrier = (fb + 24) * 10000 * (hbsel + 1);
+	fcarrier += (((u32)fc + fo) * 10) / 64 * (hbsel + 1);
+	fcarrier += ((u32)fhch * fhs * 10);
+
+	*val = fcarrier * 1000;
+
+	return 0;
+}
+
+static int si443x_fsk_set_freq(struct fsk_phy *phy, u32 val)
+{
+	struct regmap *regmap = dev_get_drvdata(phy->dev);
+	unsigned int fbsel, fo2;
+	u64 fb, fc;
+	int ret;
+
+	ret = regmap_read(regmap, REG_FBSEL, &fbsel);
+	if (ret)
+		return ret;
+	if (val >= 480000000) {
+		fbsel |= BIT(5);
+		fb = val - 480000000;
+		do_div(fb, 20000000);
+		fc = val - (24 + fb) * 20000000;
+		fc *= 2;
+		do_div(fc, (156.25 * 2 * 2));
+	} else {
+		fbsel &= ~BIT(5);
+		fb = val - 240000000;
+		do_div(fb, 10000000);
+		fc = val - (24 + fb) * 10000000;
+		fc *= 4;
+		do_div(fc, (156.25 * 4));
+	}
+	fbsel &= ~REG_FBSEL_FB_MASK;
+	fbsel |= fb & REG_FBSEL_FB_MASK;
+	ret = regmap_write(regmap, REG_FBSEL, fbsel);
+	if (ret)
+		return ret;
+
+	ret = regmap_write(regmap, REG_FC1, fc >> 8);
+	if (ret)
+		return ret;
+
+	ret = regmap_write(regmap, REG_FC2, fc & GENMASK(7, 0));
+	if (ret)
+		return ret;
+
+	ret = regmap_write(regmap, REG_FO1, 0);
+	if (ret)
+		return ret;
+
+	ret = regmap_read(regmap, REG_FO2, &fo2);
+	if (ret)
+		return ret;
+	fo2 &= ~REG_FO2_FO_MASK;
+	ret = regmap_write(regmap, REG_FO2, fo2);
+	if (ret)
+		return ret;
+
+	ret = regmap_write(regmap, REG_FHCH, 0);
+	if (ret)
+		return ret;
+
+	ret = regmap_write(regmap, REG_FHS, 0);
+	if (ret)
+		return ret;
+
+	return 0;
+}
+
+static const struct cfgfsk_ops si443x_fsk_ops = {
+	.get_freq	= si443x_fsk_get_freq,
+	.set_freq	= si443x_fsk_set_freq,
+};
 
 static struct regmap_config si443x_regmap_config = {
 	.reg_bits = 8,
@@ -35,7 +176,9 @@ MODULE_DEVICE_TABLE(of, si443x_dt_ids);
 static int si443x_probe(struct spi_device *spi)
 {
 	struct regmap *regmap;
+	struct fsk_phy *phy;
 	unsigned int val;
+	u32 freq;
 	int ret;
 
 	spi->bits_per_word = 8;
@@ -46,6 +189,8 @@ static int si443x_probe(struct spi_device *spi)
 		ret = PTR_ERR(regmap);
 		return ret;
 	}
+
+	spi_set_drvdata(spi, regmap);
 
 	ret = regmap_read(regmap, 0, &val);
 	if (ret)
@@ -58,6 +203,29 @@ static int si443x_probe(struct spi_device *spi)
 		return ret;
 
 	dev_info(&spi->dev, "device version: 0x%02x\n", val);
+
+	ret = of_property_read_u32(spi->dev.of_node, "radio-frequency", &freq);
+	if (ret) {
+		dev_err(&spi->dev, "failed reading radio-frequency");
+		return ret;
+	}
+
+	phy = devm_fsk_phy_new(&spi->dev, &si443x_fsk_ops, 0);
+	if (!phy)
+		return -ENOMEM;
+
+	ret = si443x_fsk_set_freq(phy, freq);
+	if (ret) {
+		dev_err(&spi->dev, "failed to set frequency (%d)\n", ret);
+		return ret;
+	}
+
+	ret = si443x_fsk_get_freq(phy, &freq);
+	if (ret) {
+		dev_err(&spi->dev, "failed to get frequency (%d)\n", ret);
+		return ret;
+	}
+	dev_info(&spi->dev, "frequency: %u\n", freq);
 
 	dev_info(&spi->dev, "probed\n");
 
