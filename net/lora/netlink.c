4e419f9c2b83b26f4cc7a05e95c33a2ce26c4992
Andreas Färber <afaerber@suse.de>
2019-04-29 03:16:09 +0200

    net: lora: nllora: Add get_/set_tx_power commands
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- net/lora/netlink.c
+++ net/lora/netlink.c
@@ -70,9 +70,58 @@ static int nllora_cmd_set_freq(struct sk_buff *skb, struct genl_info *info)
 	return rphy->ops->set_freq(&rphy->lora_phy, val);
 }
 
+static int nllora_cmd_get_tx_power(struct sk_buff *skb, struct genl_info *info)
+{
+	struct cfglora_registered_phy *rphy = info->user_ptr[0];
+	struct sk_buff *msg;
+	void *hdr;
+	s32 val;
+	int ret;
+
+	if (!rphy->ops->get_tx_power)
+		return -EOPNOTSUPP;
+
+	msg = nlmsg_new(NLMSG_DEFAULT_SIZE, GFP_KERNEL);
+	if (!msg) {
+		return -ENOMEM;
+	}
+
+	hdr = genlmsg_put(msg, info->snd_portid, info->snd_seq, &nllora_fam, 0, NLLORA_CMD_GET_TX_POWER);
+	nla_put_u32(msg, NLLORA_ATTR_IFINDEX, rphy->lora_phy.netdev->ifindex);
+
+	ret = rphy->ops->get_tx_power(&rphy->lora_phy, &val);
+	if (ret) {
+		genlmsg_cancel(msg, hdr);
+		nlmsg_free(msg);
+		return -ENOBUFS;
+	}
+
+	nla_put_s32(msg, NLLORA_ATTR_TX_POWER, val);
+
+	genlmsg_end(msg, hdr);
+
+	return genlmsg_reply(msg, info);
+}
+
+static int nllora_cmd_set_tx_power(struct sk_buff *skb, struct genl_info *info)
+{
+	struct cfglora_registered_phy *rphy = info->user_ptr[0];
+	s32 val;
+
+	if (!info->attrs[NLLORA_ATTR_TX_POWER])
+		return -EINVAL;
+
+	if (!rphy->ops->set_tx_power)
+		return -EOPNOTSUPP;
+
+	val = nla_get_s32(info->attrs[NLLORA_ATTR_TX_POWER]);
+	return rphy->ops->set_tx_power(&rphy->lora_phy, val);
+}
+
 static const struct nla_policy nllora_policy[NLLORA_ATTR_MAX + 1] = {
 	[NLLORA_ATTR_IFINDEX] = { .type = NLA_U32 },
 	[NLLORA_ATTR_FREQ] = { .type = NLA_U32 },
+	[NLLORA_ATTR_TX_POWER] = { .type = NLA_S32 },
 };
 
 #define NLLORA_FLAG_NEED_PHY	BIT(0)
@@ -109,6 +158,18 @@ static const struct genl_ops nllora_ops[] = {
 	{
 		.cmd = NLLORA_CMD_SET_FREQ,
 		.doit = nllora_cmd_set_freq,
+		.flags = GENL_ADMIN_PERM,
+		.internal_flags = NLLORA_FLAG_NEED_PHY,
+	},
+	{
+		.cmd = NLLORA_CMD_GET_TX_POWER,
+		.doit = nllora_cmd_get_tx_power,
+		.flags = 0,
+		.internal_flags = NLLORA_FLAG_NEED_PHY,
+	},
+	{
+		.cmd = NLLORA_CMD_SET_TX_POWER,
+		.doit = nllora_cmd_set_tx_power,
 		.flags = GENL_ADMIN_PERM,
 		.internal_flags = NLLORA_FLAG_NEED_PHY,
 	},
