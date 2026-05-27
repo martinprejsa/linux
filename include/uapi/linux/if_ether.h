BWhitten/linux-stable

net: Reserve constants for OOK and FSK
Signed-off-by: Andreas Färber <afaerber@suse.de>

include/uapi/linux/if_ether.h
@@ -149,6 +149,8 @@
 					 */
 #define ETH_P_LORA	0x00FA		/* LoRa				*/
 #define ETH_P_LORAWAN	0x00FB		/* LoRaWAN			*/
+#define ETH_P_OOK	0x00FC		/* On/Off Keying modulation	*/
+#define ETH_P_FSK	0x00FD		/* Frequency Shift Keying mod.	*/
 
 /*
  *	This is an Ethernet frame header.

martinprejsa/linux

include/uapi/linux/if_ether.h
@@ -166,6 +166,8 @@
 					 */
 #define ETH_P_LORA	0x00FB		/* LoRa				*/
 #define ETH_P_LORAWAN	0x00FC		/* LoRaWAN			*/
+#define ETH_P_OOK	0x00FD		/* On/Off Keying modulation	*/
+#define ETH_P_FSK	0x00FE		/* Frequency Shift Keying mod.	*/
 
 /*
  *	This is an Ethernet frame header.